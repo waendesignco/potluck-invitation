6285dea5eb4277b7ae54f4310aff3f36fbe7a831
ryoko-y <wagarden6@gmail.com>
2021-06-28 12:48:19 -0400

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// Change Instagram icon color
+var instagram = document.querySelector(".fa-instagram");
+instagram.style.color = "tomato";
+
+// Add background image
+var foodImage = document.querySelector(".food");
+foodImage.style.backgroundImage = "url('../img/victoria-shes-unsplash.jpg')";
+
+// Dotted border-bottom around h1
+var noms = document.querySelector("h1");
+noms.style.borderBottom = "4px dotted tomato";
